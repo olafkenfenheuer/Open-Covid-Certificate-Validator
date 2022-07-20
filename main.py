29248e56a6ce3ed8f418ea01bf04b5f8c1339c2c
Olaf Kenfenheuer <45760479+olafkenfenheuer@users.noreply.github.com>
2022-07-20 16:17:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 async def app(scope, receive, send):
     pass
 if __name__ == "__main__":
-    uvicorn.run("occv:app", host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run("occv:app", host="0.0.0.0", port=8001, log_level="info")
